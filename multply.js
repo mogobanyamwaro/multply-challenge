642b81f26c9cdca5c980ad7be4e17085173f0a6a
Douglas Mogoba Nyamwaro <mogobanyamwaro@gmail.com>
2021-05-27 16:43:16 +0300

    functions

--- multply.js
+++ multply.js
@@ -1,10 +1,34 @@
-function multiply() {
-  const args = [...arguments];
-  const result = args.reduce((total, item) => {
-    return item ? item * total : item + total;
-  }, 1);
-  return result;
+// let name = 'mosh';
+// let age = 30;
+// let isApproved = null;
+// let selectedColor;
+
+// console.log(selectedColor);
+
+// dynamic language;
+// let name = 'mosh';
+
+// let person = {
+//   name: 'mogoba',
+//   age: 30,
+// };
+// console.log(person);
+// person.name = 'john'
+// person['name'] = 'mary';
+
+// let selectedC = ['red'];
+// selectedC[1] = 'grean';
+// console.log(typeof selectedC);
+
+// function greet(name = 'mogoba') {
+//   console.log('hello ' + name);
+//   return;
+// }
+// greet('dougy');
+
+function square(number) {
+  return number * number;
 }
 
-// console.log(multiply(2, 3, 4));
-console.log(multiply(2, 0, 4));
+let number = square(2);
+console.log(number);
